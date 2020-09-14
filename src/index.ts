6643ea584fd22c597dca1d08a7d4cba0f9827b7f
Ben Botvinick <benbotvinick@gmail.com>
2020-09-14 14:56:38 -0400

    Fix code style and add showIntercomWindow

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 declare global {
   interface Window {
@@ -7,13 +7,16 @@ declare global {
   }
 }
 
-type ScriptType = "defer" | "async";
+type ScriptType = 'defer' | 'async';
 
 interface IntercomProps {
   [x: string]: any;
+
   appId?: string;
   ssr?: boolean;
+
   callBack?(param?: any): any;
+
   delay?: number;
   email?: string | null;
   name?: string;
@@ -22,7 +25,7 @@ interface IntercomProps {
   scriptInitDelay?: number;
 }
 
-let APP_ID = "";
+let APP_ID = '';
 
 function setAppId(id: string) {
   if (id) {
@@ -30,9 +33,9 @@ function setAppId(id: string) {
   }
 }
 
-function updateIntercom(event: string = "update", settings: any = null) {
-  if (typeof window !== "undefined" && window.Intercom) {
-    window.Intercom("update", settings);
+function updateIntercom(event: string = 'update', settings: any = null) {
+  if (typeof window !== 'undefined' && window.Intercom) {
+    window.Intercom('update', settings);
   }
 }
 
@@ -58,7 +61,7 @@ function loadIntercom({
   email = null,
   name,
   initWindow = true,
-  scriptType = "async",
+  scriptType = 'async',
   scriptInitDelay = 0,
   ...extra
 }: IntercomProps) {
@@ -66,36 +69,37 @@ function loadIntercom({
 
   if (ssr) {
     return createIntercomSSR(appId);
-  } else if (typeof window !== "undefined") {
-    if (typeof window.Intercom === "function") {
-      updateIntercom("reattach_activator");
-      updateIntercom("update", window.intercomSettings);
-    } else if (typeof document !== "undefined") {
-      var intercomeBase: any = function () {
-        intercomeBase.c(arguments);
-      };
-      intercomeBase.q = [];
-      intercomeBase.c = function (args) {
-        intercomeBase.q.push(args);
+  } else if (typeof window !== 'undefined') {
+    if (typeof window.Intercom === 'function') {
+      updateIntercom('reattach_activator');
+      updateIntercom('update', window.intercomSettings);
+    } else if (typeof document !== 'undefined') {
+      const intercomBase: any = function () {
+        intercomBase.c(arguments);
       };
 
-      window.Intercom = intercomeBase;
+      intercomBase.q = [];
+      intercomBase.c = function (args) {
+        intercomBase.q.push(args);
+      };
 
-      var loadScript = function () {
-        var intercomLoaded = document.querySelector(
-          `script[src="${`https://widget.intercom.io/widget/${appId}`}"]`
+      window.Intercom = intercomBase;
+
+      const loadScript = function () {
+        const intercomLoaded = document.querySelector(
+          `script[src="https://widget.intercom.io/widget/${appId}"]`
         );
 
         if (!intercomLoaded) {
-          var intercomScript = document.createElement("script");
-          intercomScript.type = "text/javascript";
+          const intercomScript = document.createElement('script');
+          intercomScript.type = 'text/javascript';
 
           // set scriptType to null for no async/defer
           if (scriptType) {
             intercomScript[scriptType] = true;
           }
           intercomScript.src = `https://widget.intercom.io/widget/${appId}`;
-          var initialScript = document.getElementsByTagName("script")[0];
+          const initialScript = document.getElementsByTagName('script')[0];
 
           if (initialScript && initialScript.parentNode) {
             initialScript.parentNode.insertBefore(
@@ -115,26 +119,26 @@ function loadIntercom({
               extra
             );
 
-            if (delay && typeof delay === "number") {
+            if (delay) {
               setTimeout(() => initIntercomWindow(intercomProps), delay);
             } else {
               initIntercomWindow(intercomProps);
             }
           }
         } else {
-          console.log("intercom script already inserted");
+          console.log('intercom script already inserted');
         }
 
-        typeof callBack === "function" && callBack();
+        typeof callBack === 'function' && callBack();
       };
 
-      if (scriptInitDelay && typeof scriptInitDelay === "number") {
+      if (scriptInitDelay) {
         setTimeout(loadScript, scriptInitDelay);
       } else {
         loadScript();
       }
     } else {
-      console.warn("intercom failed to load");
+      console.warn('intercom failed to load');
     }
   }
 }
@@ -148,18 +152,24 @@ function initIntercomWindow({ appId = APP_ID, ...otherProps }): void {
     otherProps
   );
 
-  if (typeof window !== "undefined" && window.Intercom) {
+  if (typeof window !== 'undefined' && window.Intercom) {
     if (window.intercomSettings && !window.intercomSettings.app_id) {
       window.intercomSettings = intercomProps;
     }
 
-    window.Intercom("boot", intercomProps);
+    window.Intercom('boot', intercomProps);
+  }
+}
+
+function showIntercomWindow(): void {
+  if (typeof window !== 'undefined' && window.Intercom) {
+    window.Intercom('show');
   }
 }
 
 function shutdownIntercom(): void {
-  if (typeof window !== "undefined" && window.Intercom) {
-    window.Intercom("shutdown");
+  if (typeof window !== 'undefined' && window.Intercom) {
+    window.Intercom('shutdown');
     delete window.Intercom;
     delete window.intercomSettings;
   }
